b9689de61734f4b36fc8d8c446ecd9f5c4500a6f
Arne Stenkrona <arne.stenkrona@me.com>
2022-11-27 19:27:46 +0100

    Add set point light action.

--- src/engine/editor/gui_components/component/point_light_gui.h
+++ src/engine/editor/gui_components/component/point_light_gui.h
@@ -10,6 +10,42 @@
 
 namespace prt3 {
 
+class ActionSetPointLight : public Action {
+public:
+    ActionSetPointLight(
+        EditorContext & editor_context,
+        NodeID node_id,
+        PointLight const & light
+    ) : m_editor_context{&editor_context},
+        m_node_id{node_id},
+        m_light{light}
+    {
+        Scene const & scene = editor_context.scene();
+        m_original_light =
+            scene.get_component<PointLightComponent>(m_node_id).light();
+    }
+
+protected:
+    virtual bool apply() {
+        Scene & scene = m_editor_context->scene();
+        scene.get_component<PointLightComponent>(m_node_id).light() = m_light;
+        return true;
+    }
+
+    virtual bool unapply() {
+        Scene & scene = m_editor_context->scene();
+        scene.get_component<PointLightComponent>(m_node_id).light() = m_original_light;
+        return true;
+    }
+
+private:
+    EditorContext * m_editor_context;
+    NodeID m_node_id;
+
+    PointLight m_light;
+    PointLight m_original_light;
+};
+
 template<>
 void inner_show_component<PointLightComponent>(
     EditorContext & context,
@@ -18,19 +54,20 @@ void inner_show_component<PointLightComponent>(
     Scene & scene = context.scene();
     PointLightComponent & component = scene.get_component<PointLightComponent>(id);
 
-    PointLight & light = component.light();
+    PointLight light = component.light();
+    bool change = false;
 
     float* color_p = reinterpret_cast<float*>(&light.color);
 
     ImGui::PushItemWidth(160);
 
-    ImGui::ColorEdit4("albedo", color_p);
+    change |= ImGui::ColorEdit4("albedo", color_p);
 
     float rescale_factor = 10.0f;
     float step_size = 0.01f;
 
     float quadratic = rescale_factor * light.quadratic_term - step_size;
-    ImGui::DragFloat(
+    change |= ImGui::DragFloat(
         "quadratic",
         &quadratic,
         step_size,
@@ -41,7 +78,7 @@ void inner_show_component<PointLightComponent>(
     light.quadratic_term = (quadratic + step_size) / rescale_factor;
 
     float linear = rescale_factor * light.linear_term - step_size;
-    ImGui::DragFloat(
+    change |= ImGui::DragFloat(
         "linear",
         &linear,
         step_size,
@@ -52,7 +89,7 @@ void inner_show_component<PointLightComponent>(
     light.linear_term = (linear + step_size) / rescale_factor;
 
     float constant = rescale_factor * light.constant_term - step_size;
-    ImGui::DragFloat(
+    change |= ImGui::DragFloat(
         "constant",
         &constant,
         step_size,
@@ -63,6 +100,13 @@ void inner_show_component<PointLightComponent>(
     light.constant_term = (constant + step_size) / rescale_factor;
 
     ImGui::PopItemWidth();
+
+    if (change) {
+        context.editor().perform_action<ActionSetPointLight>(
+            id,
+            light
+        );
+    }
 }
 
 } // namespace prt3
