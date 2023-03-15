da5380001f2aac32a451e1052b6a41995ce11691
Arne Stenkrona <arne.stenkrona@me.com>
2023-03-15 21:03:38 +0100

    Use std::FILE in checksum calculation.

--- src/util/checksum.cpp
+++ src/util/checksum.cpp
@@ -3,65 +3,57 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <cstdio>
 
 using namespace prt3;
 
 MD5String prt3::compute_md5(char const * path) {
-    MD5 digestMd5;
+    MD5 digest_md5;
 
-    std::ifstream file;
-    std::istream* input = nullptr;
+    std::FILE * file;
+    file = std::fopen(path, "rb");
 
-    // open file
-    file.open(path, std::ios::in | std::ios::binary);
     if (!file)
     {
         std::cerr << "prt3::compute_md5: Can't open '" << path << "'" << std::endl;
         return "";
     }
 
-    input = &file;
     // each cycle processes about 1 MByte (divisible by 144 => improves Keccak/SHA3 performance)
-    constexpr size_t BufferSize = 144 * 7 * 1024;
-    std::array<char, BufferSize> buffer;
+    constexpr size_t buffer_size = 144 * 7 * 1024;
+    std::array<char, buffer_size> buffer;
     // process file
-    while (*input) {
-        input->read(buffer.data(), BufferSize);
-        std::size_t numBytesRead = size_t(input->gcount());
-        digestMd5.add(buffer.data(), numBytesRead);
+    std::size_t num_bytes_read;
+    while ((num_bytes_read = std::fread(buffer.data(), 1, buffer_size, file)) > 0) {
+        digest_md5.add(buffer.data(), num_bytes_read);
     }
     // clean up
-    file.close();
+    std::fclose(file);
     // show results
-    return MD5String{digestMd5.getHash().c_str()};
+    return MD5String{digest_md5.getHash().c_str()};
 }
 
 CRC32String prt3::compute_crc32(char const * path) {
-    CRC32 digestCRC32;
+    CRC32 digest_crc32;
 
-    std::ifstream file;
-    std::istream* input = nullptr;
+    std::FILE * file;
+    file = std::fopen(path, "rb");
 
-    // open file
-    file.open(path, std::ios::in | std::ios::binary);
     if (!file)
     {
-        std::cerr << "prt3::compute_md5: Can't open '" << path << "'" << std::endl;
+        std::cerr << "prt3::compute_crc32: Can't open '" << path << "'" << std::endl;
         return "";
     }
 
-    input = &file;
     // each cycle processes about 1 MByte (divisible by 144 => improves Keccak/SHA3 performance)
-    constexpr size_t BufferSize = 144 * 7 * 1024;
-    std::array<char, BufferSize> buffer;
+    constexpr size_t buffer_size = 144 * 7 * 1024;
+    std::array<char, buffer_size> buffer;
     // process file
-    while (*input) {
-        input->read(buffer.data(), BufferSize);
-        std::size_t numBytesRead = size_t(input->gcount());
-        digestCRC32.add(buffer.data(), numBytesRead);
+    std::size_t num_bytes_read;
+    while ((num_bytes_read = std::fread(buffer.data(), 1, buffer_size, file)) > 0) {
+        digest_crc32.add(buffer.data(), num_bytes_read);
     }
-    // clean up
-    file.close();
+    std::fclose(file);
     // show results
-    return CRC32String{digestCRC32.getHash().c_str()};
+    return CRC32String{digest_crc32.getHash().c_str()};
 }
